0acf94546fd76f77a327b3f3c1b4f02e92e5efb8
Kevin Quinn <quinn@crc-dot1x-nat-10-239-201-133.bu.edu>
2026-05-15 14:46:53 -0400

    EM rescaling

--- analysis/synthetic_probe_test3.py
+++ analysis/synthetic_probe_test3.py
@@ -317,8 +317,16 @@ def compute_predictions(judge_models, datasets, probe_type, load_from_precompute
                             ], axis=1)
                             probe_probs = pd_data['probe'].predict_proba(X)[:, 1]
 
+                    cal_probe_probs, pi_te = rescale_probabilities_em(
+                        probe_probs, pi_tr=pd_data['train_prevalence']
+                    )
+
+                    cal_ntp_probs, pi_te = rescale_probabilities_em(
+                        ntp_probs, pi_tr=ntp_cal_data['train_prevalence']
+                    )
+
                     setting_results[dataset_type][judge_model][train_ds][test_ds] = {
-                        'probe_probs': probe_probs, 'ntp_probs': ntp_probs, 'labels': labels,
+                        'probe_probs': cal_probe_probs, 'ntp_probs': cal_ntp_probs, 'labels': labels,
                         'edges': test_edges, 'n_ground_truth': n_ground_truth,
                     }
 
