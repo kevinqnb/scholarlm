787989eb9320c15d804da130c0eba1c984907506
Kevin Quinn <quinn@crc-dot1x-nat-10-239-206-253.bu.edu>
2026-06-29 11:24:40 -0400

    Load papers utility fix.

--- src/scholarlm/utils/fs.py
+++ src/scholarlm/utils/fs.py
@@ -14,13 +14,8 @@ def get_filenames_in_directory(
     Returns:
         list: A list of filenames in the directory.
     """
-    try:
-        filenames = [
-            f for f in os.listdir(directory_path)
-            if os.path.isfile(os.path.join(directory_path, f)) and (ignore is None or f not in ignore)
-        ]
-        return filenames
-    except FileNotFoundError:
-        return f"Error: Directory not found: {directory_path}"
-    except NotADirectoryError:
-        return f"Error: Not a directory: {directory_path}"
+    filenames = [
+        f for f in os.listdir(directory_path)
+        if os.path.isfile(os.path.join(directory_path, f)) and (ignore is None or f not in ignore)
+    ]
+    return filenames
